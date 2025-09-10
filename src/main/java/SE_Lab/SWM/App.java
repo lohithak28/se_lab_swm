23d2278e9915025c56f31502772a6fca6a4b40bc
Deekshitha_Y <deekshithakmit@gmail.com>
2025-09-10 20:33:52 +0530

    all maven files

---
+++ src/main/java/SE_Lab/SWM/App.java
@@ -0,0 +1,13 @@
+package SE_Lab.SWM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Smart Waste Management System running..." );
+    }
+}
