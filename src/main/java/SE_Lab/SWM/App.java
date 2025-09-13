6e675bd8821c198696184b43edb1695e887063ac
talqs1552 <talqs1552@gmail.com>
2025-09-13 11:52:20 +0530

    intial commit

--- src/main/java/SE_Lab/SWM/App.java
+++ src/main/java/SE_Lab/SWM/App.java
@@ -4,10 +4,8 @@ package SE_Lab.SWM;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Smart Waste Management System running..." );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Smart Waste Management System running...");
     }
 }
